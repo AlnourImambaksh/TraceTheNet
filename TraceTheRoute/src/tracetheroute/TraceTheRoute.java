77452db1c9e109b141a1c5326e050e19810218dc
CiciBanane <CynthiaC@10.4.181.248>
2015-01-12 14:26:15 +0100

    Correction fct affichage

--- TraceTheRoute/src/tracetheroute/TraceTheRoute.java
+++ TraceTheRoute/src/tracetheroute/TraceTheRoute.java
@@ -16,7 +16,7 @@ public class TraceTheRoute {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        I'm coding ' !!!
+        System.out.print("I'm coding ' !!!");
     }
     
 }
