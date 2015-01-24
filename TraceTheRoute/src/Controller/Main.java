e87b41f0c567533222abf130c96b37811f0b0fd8
AlnourImambaksh <alnour28@hotmail.com>
2015-01-24 16:43:47 +0100

    Le main se viiiiddeee !!!!! \o/

--- TraceTheRoute/src/Controller/Main.java
+++ TraceTheRoute/src/Controller/Main.java
@@ -11,22 +11,6 @@ package Controller;
  */
 
 import View.View;
-import Model.Model;
-import fr.ece.fauberte.fakeroute.trace.FakeTraceGenerator;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.UnknownHostException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
     /*public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
@@ -48,38 +32,6 @@ public class Main {
         View view = new View();
         Controller controller = new Controller (view);
         controller.start();
-        Runtime r = Runtime.getRuntime();
-        Process p = null;
-        
-        try {
-            p = r.exec("tracert ece.fr");
-            
-            File file = new File("output.txt");
-            if(file.delete())
-            {
-                
-            }
-            FileOutputStream fout = new FileOutputStream("output.txt", true);
-                        
-            BufferedReader stdInput = new BufferedReader(new 
-            InputStreamReader(p.getInputStream()));
-            
-            String s = null;
-
-            while ((s = stdInput.readLine()) != null) {
-
-                PrintStream out = new PrintStream(new FileOutputStream("output.txt", true));
-                System.setOut(out);
-            System.out.println(s);
-            }
-
-            stdInput.close();
-            fout.close();
-            p.destroy();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        InputStream myStream = p.getInputStream();
      }
 }
 
